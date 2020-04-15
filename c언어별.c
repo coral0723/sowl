bd85b6d08bdc4dd56dea3e7aa54660328061827e
san9901 <san990190@gmail.com>
2020-04-15 20:27:08 +0900

    c언어 별찍기

---
+++ c언어별.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int a, b;
+
+	for (a = 1; a < 5; a++) {
+		for (b = 0; b < 5 - a; b++) {
+			printf(" ");
+		}
+		for (b = 0; b < 2 * a - 1; b++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	for (a = 3; a > 0; a--) {
+		for (b = 0; b < 5 - a; b++) {
+			printf(" ");
+		}
+		for (b = 0; b < 2 * a - 1; b++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
