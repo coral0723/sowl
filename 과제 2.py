471b43624e65409550d63b070da3eebbed9146ee
san9901 <san990190@gmail.com>
2020-04-30 01:04:32 +0900

    파이썬 과제2

---
+++ 과제 2.py
@@ -0,0 +1,2 @@
+a = 'My  naMe  is  Son  Chang  Ha:  my  pin  is  000125-3!!!!!!.'
+print(a.replace("My  naMe  is  Son  Chang  Ha:  my  pin  is  000125-3!!!!!!.","My name is Son Chang Ha, My pin is 000125-3."))
