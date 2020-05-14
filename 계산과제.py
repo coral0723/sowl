7964bfd8cef86053f5a5cd694dc12ec8ab7ff9fd
san9901 <san990190@gmail.com>
2020-05-14 13:39:37 +0900

    계산기 과제

--- 계산과제.py
+++ 계산과제.py
@@ -1,22 +1,28 @@
 while(1):
-    a1=input("정수를 입력하시오:")
+    a1=input("첫번째 정수를 입력하시오:")
     if a1=='e':
         print("종료되었습니다.")
         break;
     while a1.isalpha():
-        a1=input("정수를 다시 입력하시오:")
+        a1=input("첫번째 정수를 다시 입력하시오:")
     c=input("연산자를 입력하시오:")
     if c=='e':
         print("종료되었습니다.")
         break;
     while c.isalpha():
         c=input("연산자를 다시 입력하시오:")
-    a2=input("정수를 입력하시오:")
+        while c.isdigit():
+            c=input("연산자를 다시 입력하시오:")
+    while c.isdigit():
+        c=input("연산자를 다시 입력하시오:")
+        while c.isalpha():
+            c=input("연산자를 다시 입력하시오:")
+    a2=input("두번째 정수를 입력하시오:")
     if a2=='e':
         print("종료되었습니다.")
         break;
     while a2.isalpha():
-        a2=input("정수를 다시 입력하시오:")
+        a2=input("두번째 정수를 다시 입력하시오:")
 
     if c=='+':
         result=int(a1)+int(a2)
@@ -30,3 +36,4 @@ while(1):
         continue
 
     print(result)
+
