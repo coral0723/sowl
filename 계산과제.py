c7fd9eb1ce2e38adceac5964addb06fdbbd893ad
san9901 <san990190@gmail.com>
2020-05-14 13:33:05 +0900

    계산기 과제

--- 계산과제.py
+++ 계산과제.py
@@ -1,22 +1,22 @@
 while(1):
     a1=input("정수를 입력하시오:")
-    while a1=='c':
-        a1=input("정수를 입력하시오:")
     if a1=='e':
         print("종료되었습니다.")
         break;
+    while a1.isalpha():
+        a1=input("정수를 다시 입력하시오:")
     c=input("연산자를 입력하시오:")
-    while c=='c':
-        c=input("연산자를 입력하시오:")
     if c=='e':
         print("종료되었습니다.")
         break;
+    while c.isalpha():
+        c=input("연산자를 다시 입력하시오:")
     a2=input("정수를 입력하시오:")
-    while a2=='c':
-        a2=input("정수를 입력하시오:")
     if a2=='e':
         print("종료되었습니다.")
         break;
+    while a2.isalpha():
+        a2=input("정수를 다시 입력하시오:")
 
     if c=='+':
         result=int(a1)+int(a2)
@@ -30,4 +30,3 @@ while(1):
         continue
 
     print(result)
-
